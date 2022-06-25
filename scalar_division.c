3967312ceacc16b8b1a97bdd575506d834b5d5e9
WhyIsEveryNameTaken124 <74968938+WhyIsEveryNameTaken124@users.noreply.github.com>
2022-06-25 16:17:54 +0300

    Update scalar_division.c

--- scalar_division.c
+++ scalar_division.c
@@ -1,7 +1,9 @@
-void divide_by_scalar(int c, int r, double matrix[c][r], double scalar) {
+//NOTE: make multiply work with doubles
+
+void divideByScalar(int c, int r, double matrix[c][r], double res[c][r], double scalar) {
     if(scalar) {
         double a = 1 / scalar;
-        multiplyByScalar(r, c, matrix, a); //tuka trqbva da se razberem s ivka che leko ne bachka tui
+        multiplyByScalar(r, c, matrix, res, a);
     }
 }
 
