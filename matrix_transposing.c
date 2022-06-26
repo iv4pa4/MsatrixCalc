98a80f7bd3aea1d0c47360a5da17ac91aa5772ed
WhyIsEveryNameTaken124 <74968938+WhyIsEveryNameTaken124@users.noreply.github.com>
2022-06-26 14:00:13 +0300

    Update matrix_transposing.c

--- matrix_transposing.c
+++ matrix_transposing.c
@@ -1,4 +1,4 @@
-void transpose_matrix(int c, int r, double matrix[c][r], double res[r][c]) {
+void transposeMatrix(int c, int r, double matrix[c][r], double res[r][c]) {
     for(int i = 0; i < c; i++) {
         for(int j = 0; j < r; j++) {
             res[j][i] = matrix[i][j];
